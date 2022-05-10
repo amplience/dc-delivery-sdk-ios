1d2bcd52a7ab0ebe1a588d1c02d3cdbe35aada19
MaksPaidych <paydich28@gmail.com>
2022-05-10 22:23:56 +0300

    Initial setup

---
+++ amplience-sdk-ios/amplience-sdk-ios/amplience_sdk_ios.h
@@ -0,0 +1,18 @@
+//
+//  amplience_sdk_ios.h
+//  amplience-sdk-ios
+//
+//  Created by Maksym Paidych on 10.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for amplience_sdk_ios.
+FOUNDATION_EXPORT double amplience_sdk_iosVersionNumber;
+
+//! Project version string for amplience_sdk_ios.
+FOUNDATION_EXPORT const unsigned char amplience_sdk_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <amplience_sdk_ios/PublicHeader.h>
+
+
