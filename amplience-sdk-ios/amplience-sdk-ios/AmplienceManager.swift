dffb9e59b59ffb8c133a195d8661a8ce03c257fa
Dylan McKee <dylan@djmckee.co.uk>
2022-05-18 15:58:50 +0100

    Support fresh mode base URL

--- amplience-sdk-ios/amplience-sdk-ios/AmplienceManager.swift
+++ amplience-sdk-ios/amplience-sdk-ios/AmplienceManager.swift
@@ -14,9 +14,27 @@ class AmplienceManager {
     
     var hub: String?
     
+    var isFresh = false
+    
     var generateBaseUrl: String {
         get {
             return "https://\(hub!).cdn.content.amplience.net"
+        }
+    }
+    
+    var generateFreshBaseUrl: String {
+        get {
+            return "https://\(hub!).fresh.content.amplience.net"
+        }
+    }
+    
+    private var currentBaseUrl: String {
+        get {
+            if isFresh {
+                return generateFreshBaseUrl
+            } else {
+                return generateBaseUrl
+            }
         }
     }
     
