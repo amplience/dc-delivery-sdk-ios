4bc7f4ce9a6ca5323452bc08b54b29fbd934283c
Dylan McKee <dylan@djmckee.co.uk>
2022-05-26 13:02:15 +0100

    Improvements to the example app menu

---
+++ amplience-sdk-ios-full/MenuTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MenuTableViewCell.swift
+//  amplience-sdk-ios-full
+//
+//  Created by Dylan McKee on 26/05/2022.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var mainLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
