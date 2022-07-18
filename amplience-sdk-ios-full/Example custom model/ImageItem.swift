d742c47b557c62298eaff3fcd8ac58402546978a
MaksPaidych <paydich28@gmail.com>
2022-07-18 20:54:07 +0300

    Added linkeContent in ContentResponse

--- amplience-sdk-ios-full/Example custom model/ImageItem.swift
+++ amplience-sdk-ios-full/Example custom model/ImageItem.swift
@@ -9,15 +9,19 @@ import Foundation
 import Amplience
 
 struct ImageItem: Codable {
-    let image: AmplienceImage
-    let altText: String
+    let image: AmplienceImage?
+    let altText: String?
     
     private enum CodingKeys: String, CodingKey {
         case image, altText
     }
     
     init(dict: [String: Any]) {
-        altText = dict[CodingKeys.altText.rawValue] as! String
-        image = AmplienceImage(dict: dict[CodingKeys.image.rawValue] as! [String: Any])
+        altText = dict[CodingKeys.altText.rawValue] as? String
+        if let imageDict = dict[CodingKeys.image.rawValue] as? [String: Any] {
+            image = AmplienceImage(dict: imageDict)
+        } else {
+            image = nil
+        }
     }
 }
