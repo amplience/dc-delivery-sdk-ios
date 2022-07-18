8cd531188413a5dbf68afb7886f562997b76562b
MaksPaidych <paydich28@gmail.com>
2022-07-18 14:18:37 +0300

    Moved Page and Parameters classes in FilterContentRequest file

--- amplience-sdk-ios/amplience-sdk-ios/Models/FilterContentRequest.swift
+++ amplience-sdk-ios/amplience-sdk-ios/Models/FilterContentRequest.swift
@@ -28,3 +28,29 @@ public struct FilterContentRequest: Codable {
         self.page = page
     }
 }
+
+/**
+ * [Page] used in pagination requests
+ *
+ * @property size - The number of items to be returned in each request.
+ * The maximum page size is 12. If size is not specified then the default
+ * page size of 12 will be used.
+ *
+ * @property cursor (optional) - The starting point for the next request. Returned in
+ * the nextCursor property of the response if there are further results.
+ */
+public struct Page: Codable {
+    public let size: Int
+    public let cursor: String?
+}
+
+/**
+ * @property depth - can be root or all
+ * @property format - can be inlined or linked
+ * @property locale
+ */
+public struct Parameters: Codable {
+    public var depth: String = "all"
+    public var format: String = "inlined"
+    public var locale: String?
+}
