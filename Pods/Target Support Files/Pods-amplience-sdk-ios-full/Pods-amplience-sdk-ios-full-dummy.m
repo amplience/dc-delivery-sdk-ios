49b8c224a964fb99aedc16bca38152bda6290ef3
Dylan McKee <dylan@djmckee.co.uk>
2022-09-01 11:07:56 +0100

    Remove swiftlint as Pod dependency.

--- Pods/Target Support Files/Pods-amplience-sdk-ios-full/Pods-amplience-sdk-ios-full-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_amplience_sdk_ios_full : NSObject
-@end
-@implementation PodsDummy_Pods_amplience_sdk_ios_full
-@end
--- Pods/Target Support Files/Pods-amplience-sdk-ios-fullUnitTests/Pods-amplience-sdk-ios-fullUnitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_amplience_sdk_ios_fullUnitTests : NSObject
-@end
-@implementation PodsDummy_Pods_amplience_sdk_ios_fullUnitTests
-@end
