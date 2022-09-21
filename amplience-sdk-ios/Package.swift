bb99e4bca1759b05924ce43638e29de9b8b89071
Dylan McKee <dylan@djmckee.co.uk>
2022-09-21 18:50:03 +0300

    Added Package.swift for AmplienceDelivery

---
+++ amplience-sdk-ios/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AmplienceDelivery",
+
+    platforms: [
+        .iOS(.v13) // set your minimal version here
+    ],
+
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "amplience-sdk-ios",
+            targets: ["amplience-sdk-ios"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "amplience-sdk-ios",
+            dependencies: []),
+    ]
+)
